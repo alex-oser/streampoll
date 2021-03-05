02f935dc43db06f595a25a1357bf31411979f7bf
Alex Oser <aoser9552@gmail.com>
2021-03-04 23:50:16 -0600

    STR-12 working on twitch oauth

--- api/index.js
+++ api/index.js
@@ -1,15 +1,45 @@
 const functions = require('firebase-functions');
 const express = require('express')
-
 const admin = require('firebase-admin');
+const passport = require("passport");
+const twitchStrategy = require("passport-twitch").Strategy;
+const fs = require('fs');
+const https = require('https');
+
 admin.initializeApp();
 
 const database = admin.database();
 const config = functions.config();
+
+const privateKey = fs.readFileSync('ssl/localhost.key');
+const certificate = fs.readFileSync('ssl/localhost.crt');
+
+const credentials = { key: privateKey, cert: certificate };
+
 const app = express();
+const httpsServer = https.createServer(credentials, app);
+
+const { clientid, clientsecret } = config.twitch;
+
+passport.use(new twitchStrategy({
+  clientID: clientid,
+  clientSecret: clientsecret,
+  callbackURL: "https://127.0.0.1:3000/api/oauth/callback",
+  scope: "user_read"
+},
+function(accessToken, refreshToken, profile, done) {
+  console.log(accessToken, profile);
+}
+));
 
 app.get('/', (req, res) => {
-  res.json({ hello: 'world' });
+  res.json({ hello: 'minecraft' });
+});
+
+app.get("/login", passport.authenticate("twitch"));
+
+app.post('/api/oauth/callback', (req, res) => {
+  res.json({ hello: 'minecraft' });
 });
 
 exports.api = functions.https.onRequest(app);
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from './Login'
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
+import { Button } from '@material-ui/core';
 
 function App() {
   const [data, setData] = useState({});
@@ -15,12 +16,26 @@ function App() {
 
   }, []);
 
+  const handleLogin = () => {
+    // fetch('/api/login')
+    // .then(res => res.json())
+    // .then(res => setData(res));
+
+    window.open("http://127.0.0.1:5001/streampoll-dev-b2f18/us-central1/api/login", 'popUpWindow', 'height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+  }
 
   return (
-    <Router>      
+    <Router>
       <div className="Login">
-        <pre>{JSON.stringify(data)}</pre>
-        <Login />
+
+          <Button 
+            variant="contained"
+            size="large"
+            color="secondary"
+            onClick={handleLogin}
+          > 
+            Login 
+          </Button>
       </div>
     </Router>
   );

f437b92cae94344670b60a1edb171711b9a07caf
Alex Oser <aoser9552@gmail.com>
2021-03-04 23:57:09 -0600

    STR-12 working on twitch oauth

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     // .then(res => res.json())
     // .then(res => setData(res));
 
-    window.open("http://127.0.0.1:5001/streampoll-dev-b2f18/us-central1/api/login", 'popUpWindow', 'height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+    window.open("/api/login", 'popUpWindow', 'height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
   }
 
   return (

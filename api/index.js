b0338660743deddc1377c973b76dbed6b6279ca7
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-03-05 16:24:56 -0600

    STR-12 Test rewrite

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const express = require('express');
 const admin = require('firebase-admin');
 const passport = require('passport');
-const TwitchStrategy = require('passport-twitch').Strategy;
+const TwitchStrategy = require('passport-twitch-strategy').Strategy;
 
 admin.initializeApp();
 
@@ -12,22 +12,24 @@ const config = functions.config();
 const app = express();
 const { clientid, clientsecret } = config.twitch;
 
-const HOST = process.env.NODE_ENV === 'production' ? 'https://dev.streampoll.me' : 'https://localhost:3000';
+const HOST = process.env.NODE_ENV === 'production' ? 'https://dev.streampoll.me' : 'http://localhost:5001/streampoll-dev-b2f18/us-central1';
+console.log('function start', HOST);
+console.log('api', clientid, clientsecret);
+
+app.use(passport.initialize());
 
 passport.use(new TwitchStrategy({
   clientID: clientid,
   clientSecret: clientsecret,
-  // TODO: fix hardcode
   callbackURL: `${HOST}/api/oauth/callback`,
-  scope: 'user_read',
-},
-function(accessToken, refreshToken, profile, done) {
+  scope: 'user:read:email',
+}, (accessToken, refreshToken, profile, done) => {
   console.log(accessToken, profile);
-}
-));
+  done();
+}));
 
 app.get('/', (req, res) => res.send('hi') );
-app.get('/login', passport.authenticate('twitch'));
+app.get('/login', passport.authenticate('twitch', { forceVerify: true }));
 app.get('/oauth/callback', passport.authenticate('twitch', { failureRedirect: '/' }), (_req, res) => {
   // Successful authentication, redirect home.
   res.redirect('/');

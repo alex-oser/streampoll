499292cb27783f2ee1cfde1a6609583db6a2cc9e
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-03-07 04:23:48 +0000

    Fresh pasta

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import { nanoid } from "nanoid";
+import { useHistory } from "react-router-dom";
 
 const callbackUrl =
   window.location.protocol +
@@ -34,148 +35,129 @@ const App = () => {
     })
       .then((res) => res.json())
       .then((res) => {
-        console.log("response is " + JSON.stringify(res))
+        console.log("response is " + JSON.stringify(res));
         if (!res.error) {
-          setUserData(res)
+          setUserData(res);
         }
       });
   }, []);
 
   return (
-    <Router>
-      <div className="create-poll">
-        <Header vector={CreatePollData.headerProps.vector} logoProps={CreatePollData.headerProps.logoProps} userData={userData} loginUrl={loginUrl} />
-        <div style={{wordBreak: "break-word"}}>{loginUrl}</div>
-        <pre>{JSON.stringify(userData)}</pre>
-        <h1>{REACT_APP_BUILD_INFO}</h1>
-        <CreatePollOptions
-          basicPollProps={CreatePollData.basicPollData}
-          contestProps={CreatePollData.contestData}
-          surveyProps={CreatePollData.surveyData}
-        />
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="create-poll">
+          <Header userData={userData} loginUrl={loginUrl} />
+          <Route path="/" exact>
+            <CreatePollOptions
+              basicPollProps={null}
+              contestProps={null}
+              surveyProps={null}
+            />
+          </Route>
+          <pre style={{ position: "fixed", bottom: 20, color: "#fff " }}>
+            {REACT_APP_BUILD_INFO}
+          </pre>
+        </div>
+      </Router>
+    </>
   );
-}
+};
 
 const CreatePollOptions = (props: any) => {
-  const { basicPollProps, contestProps, surveyProps } = props;
+  const history = useHistory();
+  const handleClick = () => history.push("/test");
 
   return (
     <div>
-      <div className="choose-poll-title valign-text-middle roboto-normal-white-36px">Choose an option</div>
+      <div className="choose-poll-title valign-text-middle roboto-normal-white-36px">
+        Choose an option
+      </div>
       <PollOption
-        title={basicPollProps.title}
-        description={basicPollProps.description}
+        title="POLL (coming soon...)"
+        description="Poll with admin created options of text/images"
         isActive={false}
       />
+
       <PollOption
-        title={contestProps.title}
-        description={contestProps.description}
+        onClick={handleClick}
+        title="CONTEST"
+        description="Contest with user submission and voting periods"
       />
+
       <PollOption
-        title={surveyProps.title}
-        description={surveyProps.description}
+        title="SURVEY (coming soon...)"
+        description="Create a survey and collect results"
         isActive={false}
       />
     </div>
-      );
-}
-
+  );
+};
 
 const Header = (props: any) => {
-  const { vector, logoProps, userData, loginUrl } = props;
-  console.log("login url is " + loginUrl)
+  const { userData, loginUrl } = props;
   return (
     <div className="header">
-      <Logo
-        ellipse3={logoProps.ellipse3}
-        ellipse4={logoProps.ellipse4}
-        ellipse5={logoProps.ellipse5}
-        logonameI6961069={logoProps.logonameI6961069}
-      />
+      <Link to="/">
+        <img alt="streampoll-logo" src="/img/logo.svg" />
+      </Link>
       <div className="connect">
-        <img alt="nopers" className="vector" src={vector} />
-        <div className="text-1 valign-text-middle roboto-normal-white-36px">
-          { userData ? (
-            <a href="/api/logout">hi {userData.display_name}</a>
-          ) : (
-            <a href={loginUrl}>LOGIN</a>
-          )}
-        </div>
+        {userData ? (
+          <a className="user-link roboto-normal-white-36px" href="/api/logout">
+            {userData.display_name}
+          </a>
+        ) : (
+          <a className="user-link roboto-normal-white-36px" href={loginUrl}>
+            <img className="twitch-login-icon" alt="twitch-logo" src="/img/twitch-logo.svg" />
+            Log In
+          </a>
+        )}
       </div>
     </div>
   );
-}
-
-
-const Logo = (props: any) => {
-  const { ellipse3, ellipse4, ellipse5, logonameI6961069 } = props;
-
-  return (
-    <div className="logo">
-      <div className="overlap-group">
-        <img alt="nopers" className="ellipse-3" src={ellipse3} />
-        <img alt="nopers" className="ellipse-" src={ellipse4} />
-        <img alt="nopers" className="ellipse-" src={ellipse5} />
-      </div>
-      <h1 className="logo-name-i6961069 valign-text-middle roboto-normal-white-36px">{logonameI6961069}</h1>
-    </div>
-  );
-}
-
+};
 
 const PollOption = (props: any) => {
   const { title, description, isActive = true } = props;
 
   return (
-    <div className={"poll-option-inactive " + (isActive === true ? "poll-option-active" : "")}>
-      <div className={isActive === true ? "poll-option-active-accent" : "poll-option-inactive-accent"}></div>
+    <div
+      onClick={props.onClick}
+      className={
+        "poll-option-inactive " +
+        (isActive === true ? "poll-option-active" : "")
+      }
+    >
+      <div
+        className={
+          isActive === true
+            ? "poll-option-active-accent"
+            : "poll-option-inactive-accent"
+        }
+      ></div>
       <div className="auto-flex">
-        <div className={"poll-option-title-i12 valign-text-middle " + (isActive === true ? "roboto-normal-white-24px" : "roboto-normal-storm-dust-24px")}>
+        <div
+          className={
+            "poll-option-title-i12 valign-text-middle " +
+            (isActive === true
+              ? "roboto-normal-white-24px"
+              : "roboto-normal-storm-dust-24px")
+          }
+        >
           {title}
         </div>
-        <div className={"poll-option-text-i12 valign-text-middle " + (isActive === true ? "roboto-normal-white-18px" : "roboto-normal-storm-dust-18px")}>
+        <div
+          className={
+            "poll-option-text-i12 valign-text-middle " +
+            (isActive === true
+              ? "roboto-normal-white-18px"
+              : "roboto-normal-storm-dust-18px")
+          }
+        >
           {description}
         </div>
       </div>
     </div>
   );
-}
-
-const logoData = {
-    ellipse3: "https://anima-uploads.s3.amazonaws.com/projects/6043f71a550d896f1a967344/releases/6043f79a8cf12c0a1acfc6db/img/ellipse-3@2x.svg",
-    ellipse4: "https://anima-uploads.s3.amazonaws.com/projects/6043f71a550d896f1a967344/releases/6043f79a8cf12c0a1acfc6db/img/ellipse-3@2x.svg",
-    ellipse5: "https://anima-uploads.s3.amazonaws.com/projects/6043f71a550d896f1a967344/releases/6043f79a8cf12c0a1acfc6db/img/ellipse-3@2x.svg",
-    logonameI6961069: "streampoll",
-};
-
-const headerData = {
-    vector: "https://anima-uploads.s3.amazonaws.com/projects/6043f71a550d896f1a967344/releases/6043f79a8cf12c0a1acfc6db/img/vector@2x.svg",
-    text1: "connect",
-    logoProps: logoData,
-};
-
-const basicPollData = {
-    title: "POLL (coming soon...)",
-    description: "Poll with admin created options of text/images",
-};
-
-const contestData = {
-    title: "CONTEST",
-    description: "Contest with user submission and voting periods",
-};
-
-const surveyData = {
-    title: "SURVEY (coming soon...)",
-    description: "Create a survey and collect results",
-};
-
-const CreatePollData = {
-    headerProps: headerData,
-    basicPollData,
-    contestData,
-    surveyData,
 };
 
 export default App;

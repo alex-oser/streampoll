d7ee3dc94565d3eb6395929d836cfa3cfa4a928a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-03-14 09:28:18 -0500

    Add chat bot for twitch to get mods

--- api/routes/twitch.js
+++ api/routes/twitch.js
@@ -4,6 +4,7 @@ const got = require("got");
 const functions = require("firebase-functions");
 const config = functions.config();
 const { getTwitchUserinfo } = require("../service/twitch");
+const { getMods } = require("../service/chatbot");
 
 // /user/sodapoppin
 router.get("/user/:name", async (req, res) => {
@@ -14,6 +15,15 @@ router.get("/user/:name", async (req, res) => {
   } catch {
     res.send({ error: "no user found" });
   }
+});
+
+/**
+ * Get a list of mods in a channel by inserting a bot and running /mods
+ */
+router.get("/user/:name/mods", async (req, res) => {
+  const modList = await getMods(req.params.name);
+
+  res.send(modList);
 });
 
 exports.route = router;
---
+++ api/service/chatbot.js
@@ -0,0 +1,41 @@
+const tmi = require("tmi.js");
+
+// getting a token: https://twitchapps.com/tmi/
+// user: xd_chatbot
+// pass: minecraftspeedrun1
+
+const getMods = async (channel) => {
+  return new Promise((resolve, reject) => {
+    const client = new tmi.Client({
+      options: { debug: false, messagesLogLevel: "error" },
+      connection: {
+        reconnect: true,
+        secure: true,
+      },
+      identity: {
+        username: "xd_chatbot", // TODO: move to env
+        password: "oauth:tvossioi5mdkgmzecczbgfc138lmlu", // TODO: move to env
+      },
+      channels: [channel],
+    });
+
+    client.connect().catch(console.error);
+
+    client.on("mods", (channel, mods) => {
+      console.log("Mods", mods);
+
+      client.disconnect();
+      resolve(mods);
+    });
+
+    client.on("join", () => {
+      console.log("joined", channel)
+      client.say(channel, "/mods\n");
+    });
+
+  });
+}
+
+module.exports = {
+  getMods
+};

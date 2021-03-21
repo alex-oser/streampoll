c835e0262cfd3f642685d8efa7eec37fbdadff25
Alex Oser <aoser9552@gmail.com>
2021-03-21 12:12:09 -0500

    STR-31 work on contest gallery page

--- src/views/contest/ViewContestEntries.tsx
+++ src/views/contest/ViewContestEntries.tsx
@@ -1,19 +1,47 @@
 import { Paper, Typography } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import { DankSkelli } from "../../components/DankSkelli";
 import { getEntriesById } from "../../service/contests";
 import { useBaseStyles } from "../../style";
+import ShareIcon from '@material-ui/icons/Share';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import { ContestSettings } from "../../types/ContestSettings";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  link: {
+    "textDecoration": "none",
+    "fontWeight": "bold",
+    "display": "flex",
+    "color": theme.palette.primary.contrastText,
+    "&:hover": {
+      textDecoration: "underline",
+      cursor: "pointer"
+    },
+  },
+}));
 
 export const ViewContestEntries = (props: any) => {
   const [allEntries, setAllEntries] = useState<any>();
   const baseClasses = useBaseStyles();
-
+  const [contestData,setContestData] = useState<ContestSettings | null>(null);
   const params: any = useParams();
+  const classes = useStyles();
+  const { contestId } = params;
   useEffect(() => {
-    getEntriesById(params.contestId).then((res: any) =>
+    getEntriesById(contestId).then((res: any) =>
       setAllEntries(res)
     );
+  }, []);
+
+  useEffect(() => {
+    fetch(`/api/contest/${contestId}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setContestData(res);
+      });
   }, []);
 
   if (!allEntries) {
@@ -22,13 +50,23 @@ export const ViewContestEntries = (props: any) => {
 
   return (
     <div className={baseClasses.layout}>
-      <Typography variant="h4">ALL ENTRIES </Typography>
-      {allEntries.map((entry: any) => (
-        <Paper key={entry.entryId}>
-          <Typography variant="h5">{entry.title}</Typography>
-          <Typography variant="body2" style={{ whiteSpace: "pre-line" }}>{entry.description}</Typography>    
-        </Paper>
-      ))}
+      <Link className={classes.link} to={`/contest/${contestId}`}>
+        <Typography variant="h4">{contestData?.title || "Loading..."}</Typography>
+      </Link>
+      <div style={{ height: "100%", width: "100%", overflow: "hidden" }}>
+        <div style={{ height: "100%", width: "100%", overflow: "auto", paddingRight: 17, boxSizing: "content-box" }}>
+        {allEntries.map((entry: any) => (
+          <Paper key={entry.entryId} style={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: 15, padding: 10 }}>
+            <Typography variant="h5">{entry.title}</Typography>
+            <Typography variant="body2" style={{ whiteSpace: "pre-line" }}>{entry.description}</Typography>
+            <img src={entry.url} alt="imgur" style={{ maxHeight: 400, maxWidth: 400, objectFit: "contain" }}/> 
+            <div style={{ display: "flex" }}>
+              <FavoriteBorderIcon /> <ShareIcon />
+            </div>
+          </Paper>
+        ))}
+        </div>
+      </div>
     </div>
   );
 };
